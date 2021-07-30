aa98d366ae7ff5a7daecf5a392cd5727fee16459
Stefan Slonevskiy <stefslon@gmail.com>
2021-07-29 23:16:37 -0400

    Clean up some comments

--- httpserver.hpp
+++ httpserver.hpp
@@ -1,7 +1,27 @@
-// Simple HTTP server from https://stackoverflow.com/a/32931195
+/***************************************************************************
+//
+//    softProjector - an open source media projection software
+//    Copyright (C) 2021  Vladislav Kobzar
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation version 3 of the License.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+***************************************************************************/
 
-#ifndef HTTPSERVER
-#define HTTPSERVER
+// Simple HTTP server implementation in Qt taken from https://stackoverflow.com/a/32931195
+
+#ifndef HTTPSERVER_HPP
+#define HTTPSERVER_HPP
+
 #include <QCoreApplication>
 #include <QNetworkInterface>
 #include <iostream>
